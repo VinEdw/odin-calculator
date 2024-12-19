5d9f0df85181357bbf145fb93d440fbbfff68a62
Vincent Edwards <mister.man489@gmail.com>
2024-12-19 12:01:57 -0800

    Start breaking script up into modules

---
+++ modules/key_names.js
@@ -0,0 +1,32 @@
+// Set arrays of valid key strings, grouped into categories
+const digits = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+
+const operators = [
+  "+",
+  "-",
+  "*",
+  "/",
+];
+
+const all = [
+  ".",
+  "=",
+  "~",
+  "Backspace",
+  "Delete",
+];
+all.push(...digits);
+all.push(...operators);
+
+export { digits, operators, all }
