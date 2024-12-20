c3aad0014e03403b83d25ff3a232957dcaaa442b
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:16:12 -0800

    Create a function to update the display based on the state

--- modules/calculator.js
+++ modules/calculator.js
@@ -11,6 +11,23 @@ class Calculator {
     this.buttonContainerElement = buttonContainerElement;
     this.display = new Display(displayElement);
   }
+
+  updateDisplay() {
+    switch (this.state) {
+      case calculatorStates.firstRegisterFocused:
+        this.display.showRegister(this.firstRegister);
+        break;
+      case calculatorStates.operatorPressed:
+        this.display.showRegister(this.firstRegister);
+        break;
+      case calculatorStates.secondRegisterFocused:
+        this.display.showRegister(this.secondRegister);
+        break;
+      case calculatorStates.equalPressed:
+        this.display.showRegister(this.firstRegister);
+        break;
+    }
+  }
 }
 
 export { Calculator };
