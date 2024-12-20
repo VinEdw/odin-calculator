16a427a30dc1651ae2f383f4cd502378a39e0b27
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:38:39 -0800

    Grab the relevant HTML elements

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 import * as keyNames from "./modules/key_names.js";
+
+const buttonContainerElement = document.querySelector("#calculator-buttons");
+const displayElement = document.querySelector("#calculator-display");
