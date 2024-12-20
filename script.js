863d8599264f697e2aa8f8041de1cca06dbe949e
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:39:18 -0800

    Create a calculator object

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import * as keyNames from "./modules/key_names.js";
+import { Calculator } from "./modules/calculator.js";
 
 const buttonContainerElement = document.querySelector("#calculator-buttons");
 const displayElement = document.querySelector("#calculator-display");
+const calculator = new Calculator(buttonContainerElement, displayElement);
