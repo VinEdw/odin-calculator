fbe014e084d7e90c6da393bcfac0e3767d3111b7
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:08:00 -0800

    Store the state the calculator is in

--- modules/calculator.js
+++ modules/calculator.js
@@ -1,9 +1,11 @@
+import * as calculatorStates from "./calculator_states.js";
 import { Register } from "./register.js";
 
 class Calculator {
   firstRegister = new Register();
   secondRegister = new Register();
   operator = "";
+  state = calculatorStates.firstRegisterFocused;
 }
 
 export { Calculator };
