35a4b5d878c66301e383a65572d14a3bebed5e3f
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:04:28 -0800

    Create a function to display the value in a register

--- modules/display.js
+++ modules/display.js
@@ -2,6 +2,10 @@ class Display {
   constructor(element) {
     this.element = element;
   }
+
+  showRegister(register) {
+    this.element.textContent = register.getDisplayStr();
+  }
 }
 
 export { Display };
