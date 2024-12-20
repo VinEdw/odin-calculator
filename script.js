f27262c37f8d14ddd0465d0db0c9e5bf8dcde684
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:40:50 -0800

    Send button clicks to the calculator

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ import { Calculator } from "./modules/calculator.js";
 const buttonContainerElement = document.querySelector("#calculator-buttons");
 const displayElement = document.querySelector("#calculator-display");
 const calculator = new Calculator(buttonContainerElement, displayElement);
+
+buttonContainerElement.addEventListener("click", (e) => {
+  const el = e.target;
+  if (el.localName === "button") {
+    calculator.handleInput(el.dataset.key);
+  }
+});
