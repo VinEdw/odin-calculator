37f65b2723a201dd64b20b14b8dc30c0a827139f
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 13:41:37 -0800

    Send key presses to the calculator

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ buttonContainerElement.addEventListener("click", (e) => {
     calculator.handleInput(el.dataset.key);
   }
 });
+
+document.addEventListener("keydown", (e) => {
+  if (keyNames.all.includes(e.key)) {
+    // Prevent the Quick Find shortcut from activating in Firefox
+    if (e.key === "/") {
+      e.preventDefault();
+    }
+    calculator.handleInput(e.key);
+  }
+  else if (e.key === "Enter") {
+    // Treat Enter as an equal sign
+    calculator.handleInput("=");
+  }
+});
