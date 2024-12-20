d03387a3f9998272bb41f8381c33c56d20221e7e
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 12:51:49 -0800

    Create a function to append characters to the string

--- modules/register.js
+++ modules/register.js
@@ -1,6 +1,22 @@
+import * as keyNames from "./key_names.js";
+
 class Register {
   negative = false;
   str = "";
+
+  appendCharacter(char, maxCharacters = 15) {
+    if (this.str.length >= maxCharacters) {
+      return;
+    }
+    
+    const charIsDigit = keyNames.digits.includes(char);
+    const charIsDecimal = char === ".";
+    const strHasDecimal = this.str.includes(".");
+
+    if (charIsDigit || (charIsDecimal && !strHasDecimal)) {
+      this.str += char;
+    }
+  }
 }
 
 export { Register };
