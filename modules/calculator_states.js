80db99e08eb13ea84aabfccb1981973f7a32f305
Vincent Edwards <mister.man489@gmail.com>
2024-12-20 12:46:42 -0800

    Define numbers for the possible calculator states

---
+++ modules/calculator_states.js
@@ -0,0 +1,6 @@
+const firstRegisterFocused = 0;
+const operatorPressed = 1;
+const secondRegisterFocused = 2;
+const equalPressed = 3;
+
+export { firstRegisterFocused, operatorPressed, secondRegisterFocused, equalPressed };
